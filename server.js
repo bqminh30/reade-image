774b04a2962a4c95555c29c0744fdda447599e99
bqminh <64219602+bqminh30@users.noreply.github.com>
2024-01-30 17:30:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 7000;
 const router = require('./router');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
